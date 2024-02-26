b2d5b4dbb94733a0f78c86459a8ad184f23032a6
EnricoVojtech <enrico.vojtech@gmail.com>
2024-02-26 10:52:48 +0100

    start pipeLife

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,26 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { LoginComponent } from './login/login.component';
+import {ReactiveFormsModule} from "@angular/forms";
+import {ButtonModule} from "primeng/button";
+import {InputTextModule} from "primeng/inputtext";
+import {NgOptimizedImage} from "@angular/common";
+import { HomeComponent } from './home/home.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent,
+    HomeComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    ReactiveFormsModule,
+    ButtonModule,
+    InputTextModule,
+    NgOptimizedImage
   ],
   providers: [],
   bootstrap: [AppComponent]
