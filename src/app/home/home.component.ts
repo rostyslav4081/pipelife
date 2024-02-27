7565782582c16ab9dd967bc19080e9c19d2ee3bb
EnricoVojtech <enrico.vojtech@gmail.com>
2024-02-27 12:52:59 +0100

    add3!

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,4 +1,5 @@
 import {Component} from '@angular/core';
+import {FormBuilder, FormGroup, Validators} from "@angular/forms";
 
 @Component({
   selector: 'app-home',
@@ -6,5 +7,35 @@ import {Component} from '@angular/core';
   styleUrl: './home.component.css'
 })
 export class HomeComponent {
+  fullName!: string;
+  SPZ!: string;
+  showBtn1 = {
+    start: true,
+    firstDiv: false,
+    next: false
+  };
+  showBtn2 = {
 
+    secondDiv: false,
+  };
+  postForm!: FormGroup;
+
+  constructor(
+    private fb: FormBuilder,
+    // public ref: DynamicDialogRef,
+    // public config: DynamicDialogConfig,
+    // private messageService: MessageService
+  ) {
+
+    this.postForm = this.fb.group({
+      id: undefined,
+      fullNameForm: ['', Validators.required],
+      SPZForm: ['', Validators.required],
+    });
+
+  }
+
+  postData() {
+
+  }
 }
